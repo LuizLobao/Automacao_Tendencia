3853205d62529b3e49dae9210702bd4f82596157
LuizLobao <luiz@lobao.com>
2022-10-26 19:47:11 -0300

    definindo estrategia de LOOP

--- testes.py
+++ testes.py
@@ -7,7 +7,7 @@ import win32com.client as win32
 import plotly.express as px
 
 hoje = datetime.today().strftime('%d/%m/%Y')
-print(hoje)
+agora = datetime.today()
 
 dados_conexao = (
         "Driver={SQL Server};"
@@ -22,6 +22,13 @@ print("Conectado")
 cursor = conexao.cursor()
 
 #colocar aqui um loop verificando se a procedure terminou de rodar
+# gerar variavel com a datahora de inicio de execuçao inicio = datetime.today()
+# a procedure ira gravar a hora de inicio e fim da execução na tabela TBL_PC_TEMPO_PROCEDURES
+# puxar a hora FIM da procedure
+# criar um loop que ira verificar se a hora de fim da procedure for MAIOR que a variavel INICIO
+# se MENOR, continuar esperando
+# se MAIOR, continuar com o processo
+
 print('Iniciando LOOP para verificar se a Procedure terminou de rodar')
 comando_sql = '''SELECT [PROCEDURE], INI_FIM, MAX(DATA_HORA) AS DATA_HORA
 				  FROM TBL_PC_TEMPO_PROCEDURES
@@ -32,9 +39,11 @@ comando_sql = '''SELECT [PROCEDURE], INI_FIM, MAX(DATA_HORA) AS DATA_HORA
 cursor.execute(comando_sql)
 row = cursor.fetchone()
 print(row)
+print(row.DATA_HORA)
 print(row.DATA_HORA.strftime('%Y%m%d'))
+print(agora)
 
-
+print(row.DATA_HORA>agora)
 
 conexao.close()
 print('Conexão Fechada')
