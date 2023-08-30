e5ea37483892a1edf359fb13fe3f21b21c8804bd
Luiz Lobão <luiz@lobao.com>
2023-08-29 22:55:22 -0300

    nao sei

--- teste-json.py
+++ teste-json.py
@@ -1,42 +1,6 @@
-from datetime import date, datetime, timedelta
-import json
+from math import ceil
 
-hoje = (datetime.today()- timedelta(days=0)).strftime('%d/%m/%Y') 
-dicio = {hoje: [
-                {
-                "BOV_1066.TXT":'01/01/1900 00:00:00',
-                "BOV_1065.TXT":'01/01/1900 00:00:00',
-                "BOV_1059.TXT":'01/01/1900 00:00:00',
-                "BOV_1067.TXT":'01/01/1900 00:00:00',
-                "BOV_1064.TXT":'01/01/1900 00:00:00',
-                "BOV_1058.TXT":'01/01/1900 00:00:00',
-                "HADOOP_6162.TXT":'01/01/1900 00:00:00',
-                "HADOOP_6163.TXT":'01/01/1900 00:00:00'
-                }
-                ]
-        }
-#print(dicio)
+receita = 1000
+unidade = 15
 
-json_string = json.dumps(dicio, indent=2, sort_keys=True)
-
-with open('cargas.json','a') as f:
-    f.write(json_string)
-
-
-with open('cargas.json', 'r') as g:
-    dicionario = json.loads(g.read())
-
-print(dicionario)
-print('')
-print('')
-print(dicionario.keys())
-print('')
-print('')
-print(dicionario['22/03/2023'][0])
-print('')
-print('')
-print(dicionario['22/03/2023'][0]['BOV_1065.TXT'])
-print('')
-print('')
-print(dicionario['22/03/2023'][0].keys())
-print(dicionario['22/03/2023'][0].values())
+print(f" A empresa vendeu aproximadamente {ceil(receita/unidade)} unidades")
