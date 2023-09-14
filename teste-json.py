b399aa61235de354cd869bd41a31c01e3a2aeb98
LuizLobao <luiz@lobao.com>
2023-09-14 16:57:05 -0300

    Excluindo arquivos de testes

--- teste-json.py
+++
@@ -1,6 +0,0 @@
-from math import ceil
-
-receita = 1000
-unidade = 15
-
-print(f" A empresa vendeu aproximadamente {ceil(receita/unidade)} unidades")
--- teste_csv_pandas.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-arquivo = 'pdv_canal_vl_junho23_1a18_rcs.csv'
-
-df = pd.read_csv(arquivo, delimiter=';')
-
-print(df.head())
